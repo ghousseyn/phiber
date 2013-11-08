bc437f8e30595f248c24e42856dd4d44c15c8136
ghussein <ghussein@coda-dz.com>
2013-11-08 18:16:35 +0100

    Adding fresh framework files

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+include "main.class.php";
+$main = main::getInstance();
+
+$debug = $main->load('debug');
+$debug->start();
+/*
+$conf = $main->load('config');
+
+
+try{
+	$db = $main->load('db',$conf);
+	
+}catch(Exception $e){
+	echo $debug;
+	print $e->getMessage();
+}
+*/
+//$db->reset();
+
+//$db->insert(array("entries",array("name" => "guettaf","fname" => "hussein","street" => "my street","zip" => "32000","city" => 2)));
+//$db->update(array("entries",array("name" => "guettaf", "fname" => "nour", "city" => 5), "id=3"));
+//$db->delete(array("entries",array("id=2")));
+//$db->select(array("entries as en", array("en.id", "en.name","en.fname","en.street","en.zip", "city.name as city"), " LEFT JOIN city ON en.city = city.id"));
+//print_r($db);
+//echo $conf->_dbuser;
+?>
+<br />
+<pre>
+<?php 
+$main->run();
+
+//$route = $main->getRoute();
+//echo $main->dispatch();
+//echo "Module: ".$route['module']."<br />";
+//echo "Controller: ".$route['controller']."<br />";
+//echo "Action: ".$route['action']."<br />";
+$debug->stackPush(__file__);
+//var_dump($main->get("config")->_dbhost);
+echo $debug;
+
+
+
+
+
+
+?>
+</pre>
---
+++ modules/dev/class.apc.php
@@ -0,0 +1,22 @@
+<?php
+class apc extends main {
+
+	function init(){
+
+	}
+	static function getInstance(){
+		return new apc;
+	}
+	function act(){
+		if ($quote = apc_fetch('star')) {
+  			 $quote .= " [cached]";
+  			
+		} else { 
+			$quote = "Do, or do not. There is no try. -- Yoda, Star Wars";  
+	  		
+	 		 apc_add('star', $quote, 120);
+		}
+		$this->_view()->dbg = $this->get('_request');
+		$this->_view()->var = $this->_request("var");
+	}
+}
