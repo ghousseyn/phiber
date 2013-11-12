0db724da76d3073f84ead705c247c1117843be44
ghussein <ghussein@coda-dz.com>
2013-11-12 12:51:18 +0100

    cleaning up

--- library/layouts/layout.php
+++ library/layouts/layout.php
@@ -1,22 +1,7 @@
-<html>
-<head>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-	$("#click").click(function(){
-		$.get('index/index',function(resp){
-			alert(resp);
-		});
-	});
-});
-</script>
-</head>
-<body>
 <?php
 include $this->content;
 
 echo $this->debug;
 
 ?>
-</body>
-</html>
+
