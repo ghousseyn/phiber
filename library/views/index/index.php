f7ef197d3030b34d3a269440dab64c8e52540651
Guettaf Hussein <GHussein@coda-dz.com>
2014-02-13 06:36:00 +0100

    added links to the test module on the default first page as examples showing also some paths

--- library/views/index/index.php
+++ library/views/index/index.php
@@ -1,5 +1,11 @@
+Template file:  
+<?php
 
-<button id="click">click</button>
+echo __file__;
+
+?>
+<br/>
+Examples: <a href="/dev" title="Defaults to controler index and action index">Dev</a> | <a href="/dev/index/action" title="controller index action action">Dev:action</a> | <a href="/dev/index!" title="See errors bellow">invalid</a><br/>
 <?php
 echo $this->text;
 ?>
