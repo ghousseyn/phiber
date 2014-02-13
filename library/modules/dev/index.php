19828e01474fa8309e17594a3630a60a3c4501fc
housseyn guettaf <ghoucine@gmail.com>
2014-02-13 06:12:08 +0100

    bottom bar color change for better visibility

--- library/modules/dev/index.php
+++ library/modules/dev/index.php
@@ -6,15 +6,15 @@ class index extends Codup\main
     function index ()
     {
 		$this->view->message = "Hello world";
-		var_dump($this->get('_request'));
+		
     }
 
     function action ()
     {
         $this->stack("dev:action");
-        $this->db->select(array('translation', array('*'), ''));
+        //$this->db->select(array('translation', array('*'), ''));
         
-        $this->view->text = "test";
+        $this->view->text = "text from the dev module controller";
         $this->view->origin = __class__;
         $this->view->file = __file__;
     }
