5bcbdbd4fce7366e6614cd8b24246aac41ba6425
housseyn guettaf <ghoucine@gmail.com>
2014-02-28 21:28:35 +0100

    moved oosql dsn to config file tested PSR compliant automatic code formating removed try catch block from the oosql construct replaced error returns with PDOExceptions

--- library/modules/dev/index.php
+++ library/modules/dev/index.php
@@ -18,7 +18,7 @@ public static $test = 2;
 	$this->view->origin = __class__;
         $this->view->file = __file__;
 	$this->view->text = "welcome to action";
-//$this->tools->wtf($_SESSION);
+$this->tools->wtf($_SESSION);
 	if($this->isAjax()){
         	$this->sendJSON(array(0=>"this thing",1=>"another"));
 		$this->contextSwitch('html');
--- library/plugins/context/context.php
+++ library/plugins/context/context.php
@@ -2,36 +2,36 @@
 
 class context extends Codup\main
 {
-    
-   
+
+
     function run ()
     {
 
-    	//$managers = models\dept_emp::getInstance();
-    	//$results = $managers->select()->fetch(200);
+    	$managers = models\dept_emp::getInstance();
+    	$results = $managers->select()->fetch(200);
     	/*foreach($results as $result){
     		$result->from_date = "1983-03-26";
     		$result->save();
     	}
     	*/
-    	//echo $results->count();
-	//$col = $results->findAll('dept_no','d009');
-	//$col->sortbyproperty('from_date');
-    	//$this->tools->wtf($col);
+    	echo $results->count();
+	$col = $results->findAll('dept_no','d009');
+	$col->sortbyproperty('from_date');
+    	$this->tools->wtf($col);
 	//$this->tools->wtf($results->findAll('dept_no','d007'));
     	/*$depts = models\titles::getInstance();;
-    	
+
     	try {
-    		
+
     		$titles = $depts->select()->fetch(2);
     		$title = $titles(0);
     		$title->to_date = "1986-06-28";
     		$title->save();
-    	
+
     	}catch (Exception $e){
-    		
+
     		$this->tools->wtf($e);
-    	
+
     	}
     	$this->tools->wtf($titles);
         $dept_manager = models\dept_manager::getInstance();
@@ -41,58 +41,58 @@ class context extends Codup\main
          //$res = $test->find(array('last_name' => "Mal%"),'like')->fetch();
          // or
 	     //$res = $test->select()->where('last_name like ?',"Mal%")->fetch();
-	  
+
          /*
-          * 
-          * SELECT 	dept_manager.dept_no, 
-          * 		dept_manager.emp_no, 
-          * 		dept_manager.from_date, 
+          *
+          * SELECT 	dept_manager.dept_no,
+          * 		dept_manager.emp_no,
+          * 		dept_manager.from_date,
           * 		dept_manager.to_date ,
           * 		departments.dept_name ,
-          * 		employees.last_name 
-          * FROM 	dept_manager 
-          *     JOIN 	departments 
-          *       ON 		dept_manager.dept_no = departments.dept_no 
-          *     JOIN 	employees 
-          *       ON 		dept_manager.emp_no = employees.emp_no 
+          * 		employees.last_name
+          * FROM 	dept_manager
+          *     JOIN 	departments
+          *       ON 		dept_manager.dept_no = departments.dept_no
+          *     JOIN 	employees
+          *       ON 		dept_manager.emp_no = employees.emp_no
           * WHERE dept_manager.emp_no = 110039
-          * 
+          *
           * $res = $dept_manager->find(110039)
 								->with(array('departments' => array("dept_name")))
 								->with(array('employees' => array("last_name")))
 								->fetch();
-          * 
-          * 
+          *
+          *
           * ============================================================================
-          * 
-          * 
+          *
+          *
           * SELECT 	dept_manager.*,
           * 		departments.dept_name ,
-          * 		employees.last_name 
-          * FROM 	dept_manager 
-          *     JOIN 	departments 
-          *       ON 		dept_manager.dept_no = departments.dept_no 
-          *     JOIN 	employees 
-          *       ON 		dept_manager.emp_no = employees.emp_no 
+          * 		employees.last_name
+          * FROM 	dept_manager
+          *     JOIN 	departments
+          *       ON 		dept_manager.dept_no = departments.dept_no
+          *     JOIN 	employees
+          *       ON 		dept_manager.emp_no = employees.emp_no
           * LIMIT 	0,20
           */
     	/*
 		$deps = models\dept_emp::getInstance();
-         
+
          //$deps->delete(array("dept_no = ?", "d012"));
-         
+
 		  $res = $dept_manager->findAll()//or $dept_manager->findAll()
 							  ->with(array('departments'=>array("dept_name"),'employees'))
 							  ->fetch(20);
-		  
+
 		  $this->tools->wtf($res);
 		  $res2 = $deps->findAll()->fetch(30);
-		  
+
 		  $res2->removeWhere('dept_no','d004');
 		  $res2->removeWhere('dept_no','d005');
 		  $res2->restoreWhere('dept_no','d005');
 		  $res2->sortByProperty('from_date');
-		
+
 		  $keys = $res2->objectWhere('dept_no','d005');
 		  $this->tools->wtf($res2);
 		  $dept2 = $res2(0)->departments()->load();
@@ -103,7 +103,7 @@ class context extends Codup\main
 		  $this->tools->wtf($dept2);
 		  echo $res;
 		  echo $res2;
-		 
+
          //($deps);
          //var_dump($test->errors);
 
@@ -111,42 +111,42 @@ class context extends Codup\main
        // var_dump($res);
 
        // var_dump($res);
-         
+
       //echo count($res);
-         
+
        // foreach( $res as $record){
         	//foreach($record as $key => $rec){
         	//	echo $record->last_name."</br>";
         	//}
       //  }
-        
+
         $dept = $res(1)->employees();
-    
+
         $this->tools->wtf($dept);
         $dept->first_name = "hocine";
         $dept->last_name = "guettaf";
         $dept->gender = 'M';
         $res(1)->dept_no = 'd004';
-        
+
         $dept->save();
         $res(1)->save();
         //$this->tools->wtf($dept);
         //$dept->dept_no = "d018";
          //won't work (trying to change a primary key here) try using update() instead BAD idea though!
          //$dept->dept_no = "d010";
-         
+
 		  //$this->tools->wtf($res2);
      /*
-         
+
        //  echo self::$t;
          if(!$res){
              var_dump($test->geterrors);
              return;
          }
          while ( $r = $res->iterate()){
-         //$test2 = (array) $res; 
+         //$test2 = (array) $res;
          //echo $r->last_name." ".$r->first_name."<br/>";
-         
+
         // $employees = $r->employees();
         // if($employees->gender == "M"){
          //	$employees->gender = "F";
@@ -154,20 +154,20 @@ class context extends Codup\main
         // $employees->save();
          echo $r->dept_name." ".$r->last_name." ".$r->first_name." ".$r->gender."</br>";
          //$r->cms_componenttype_id = 7;
-         //$r->is_enabled = 'Y';    
+         //$r->is_enabled = 'Y';
          //$r->dept_name = "Made up dept";
          //$r->save();
 
          }*/
          //var_dump($res);
-        
+
         // ;
         /*
-         
+
 
            */
         if ($this->isAjax()) {
-            
+
         }
     }
 }
