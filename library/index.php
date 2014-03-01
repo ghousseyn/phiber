5597a720ce6b6f0bc408f8c104d1b9f267ed0920
comptabilite <comptabilite@cbnx-1763b98e3a>
2014-03-01 19:48:14 +0100

    Reduced Cyclomatic complexity of main::router()

--- library/index.php
+++ library/index.php
@@ -5,14 +5,14 @@ class index extends Codup\main
 
   function index()
   {
-    
+
     $this->view->text = "Text from default controller: " . __file__;
   }
 
   function action()
   {
     $this->stack("default:" . __method__);
-  
+
   }
 }
 
