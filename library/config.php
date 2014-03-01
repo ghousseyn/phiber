2a5ad45abe4ae74d4eb9cbf1b6026de6e7dc9838
housseyn guettaf <ghoucine@gmail.com>
2014-03-01 03:42:57 +0100

    Added documentation to oosql class cleaned code off of main

--- library/config.php
+++ library/config.php
@@ -9,7 +9,7 @@
 
 class config
 {
-  
+
   /*
    * Path to the library relative to this file (set in the construct)
    */
@@ -19,33 +19,33 @@ class config
    * to true for a default value of 1800 seconds (30 minutes) Or set it to
    * whatever value you like to set in seconds
    */
-  protected $sessionReginerate = true;
+  protected $sessionReginerate = false;
   /*
    * If set to true the session will be destroyed after 1800 seconds (30
    * minutes) of inactivity Alternatively set the value that you like in seconds
    * Set it to false to disable the functionality
    */
-  protected $inactive = true;
+  protected $inactive = false;
   /*
    * The action method that should be called from your controller in case a
    * none-existant action is called (or none specified)
    */
   protected $defaultMethod = 'main';
-  
+
   /*
    * Enable/disable debug
    */
   public $debug = false;
-  
+
   /*
    * DB configuration properties
    */
   protected $_dsn = 'mysql:host=127.0.0.1;dbname=codup';
-  
+
   protected $_dbpass = "hggiHmfv";
-  
+
   protected $_dbuser = "root";
-  
+
   protected $layoutEnabled = true;
 
   protected function __construct()
@@ -64,11 +64,12 @@ class config
    */
   function __get($var)
   {
-    if(key_exists($var, get_class_vars(__CLASS__))){
-      
+
+    if(property_exists(__CLASS__, $var)){
+
       return $this->{$var};
     }
-  
+
   }
 
 }
--- library/main.php
+++ library/main.php
@@ -10,18 +10,18 @@ namespace Codup;
 
 class main
 {
-  
+
   private $path = null;
-  
+
   protected $_requestVars = array();
-  
+
   protected $vars = array();
 
   protected function __construct()
   {
     spl_autoload_register(array($this, 'autoload'));
     if($this->conf->debug){
-      
+
       $this->debug->start();
     }
     $this->register('layoutEnabled', $this->conf->layoutEnabled);
@@ -29,12 +29,12 @@ class main
       session_start();
       $_SESSION['user']['activity'] = time();
     }
-  
+
   }
   /*
    * Implement the getInstance() method
    */
-  
+
   static function getInstance()
   {
     return new static();
@@ -42,7 +42,7 @@ class main
 
   function run()
   {
-    
+
     $this->checkSession();
     $this->register('post', false);
     $this->register('get', true);
@@ -57,13 +57,13 @@ class main
     }
     if($this->isAjax()){
       if($this->get('context') == 'html'){
-        
+
         $this->register('layoutEnabled', false);
       }
     }
-    
+
     $this->view->showTime();
-  
+
   }
 
   function _redirect($url, $replace = true, $code = 307)
@@ -73,7 +73,7 @@ class main
 
   function plugins()
   {
-    
+
     foreach($this->bootstrap->getPlugins() as $plugin){
       $this->load($plugin, null, $this->conf->library . "/plugins/" . $plugin . "/")->run();
     }
@@ -83,7 +83,7 @@ class main
   {
     if($this->conf->inactive){
       if(isset($_SESSION['user']['activity']) && (time() - $_SESSION['user']['activity'] > $this->load('tools')->orDefault((int) $this->conf->inactive, 1800))){
-        
+
         // session_unset();
         session_destroy();
       }
@@ -94,25 +94,25 @@ class main
         $_SESSION['user']['created'] = time();
       }
     }
-  
+
   }
 
   function getView()
   {
-    
+
     $path[] = $this->route['module'];
     $path[] = $this->route['controller'];
     $path[] = $this->route['action'];
-    
+
     if($path[0] == 'default'){
       array_shift($path);
       $path = $this->conf->library . "/views/" . implode("/", $path) . ".php";
     }else{
       $path = $this->conf->library . "/modules/" . array_shift($path) . "/views/" . implode("/", $path) . ".php";
     }
-    
+
     $this->view->viewPath = $path;
-  
+
   }
 
   function renderLayout($layout = null)
@@ -121,7 +121,7 @@ class main
       if(file_exists($layout)){
         include_once $layout;
       }
-    
+
     }else{
       include_once $this->conf->library . "/layouts/layout.php";
     }
@@ -130,91 +130,93 @@ class main
   function router()
   {
     $uri = urldecode($_SERVER['REQUEST_URI']);
-    
+
     if($this->isValidURI($uri)){
-      
+
       $parts = explode("/", $uri);
-      
+
       array_shift($parts);
-      
+
       if(! empty($parts[0]) && $this->bootstrap->isModule($parts[0])){
-        
+
         $module = array_shift($parts);
-        
+
         if($this->hasController($parts, $module)){
-          
+
           $controller = array_shift($parts);
-        
+
         }else{
-          
+
           $controller = "index";
-          
+
           array_shift($parts);
-        
+
         }
-        
+
         if($this->hasAction($parts, $controller, $module)){
-          
+
           $action = array_shift($parts);
-        
+
         }else{
           $action = $this->conf->defaultMethod;
-          
+
           array_shift($parts);
-        
+
         }
       }else{
-        
+        if(empty($parts[0])){
+          goto shortcut;
+        }
         $module = "default";
-        
+
         array_shift($parts);
-        
+
         if($this->hasController($parts, $module)){
-          
+
           $controller = array_shift($parts);
-        
+
         }else{
           $controller = "index";
           // array_shift($parts);
         }
-        
+
         if($this->hasAction($parts, $controller, $module)){
-          
+
           $action = array_shift($parts);
-        
+
         }else{
           $action = "index";
           array_shift($parts);
         }
-      
+
       }
-      
+
       if(count($parts)){
-        
+
         $this->setVars($parts);
-      
+
       }
-      
+
       if($_POST){
         $this->register('post', true);
         $this->register('get', false);
         $this->_requestVars = $_POST;
       }
-      
+
       $this->register('_request', $this->_requestVars);
-      
+
       $route = array("module" => $module, "controller" => $controller, "action" => $action, "vars" => $this->get('_request'));
       if(! empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
         $this->register('ajax', true);
       }
-    
+
     }else{
-      
+      shortcut:
       $route = array("module" => "default", "controller" => "index", "action" => "index");
-    
+
     }
     $this->register('route', $route);
-  
+
   }
 
   function isValidURI(&$uri)
@@ -224,7 +226,7 @@ class main
     $uri = str_replace("/?", "/", $uri);
     $uri = str_replace("&", "/", $uri);
     $uri = str_replace("=", "/", $uri);
-    
+
     if(preg_match("~^(?:[/\\w\\s-]+)+/?$~", $uri)){
       return true;
     }
@@ -256,29 +258,29 @@ class main
     // $mod = $this->route["module"];
     $controller = $this->route["controller"];
     $action = $this->route["action"];
-    
+
     $instance = $this->load($controller, null, $this->path);
-    
-    if(in_array($action, get_class_methods($instance))){
-      
+
+    if(method_exists($instance, $action)){
+
       $instance->{$action}();
     }else{
       // $this->errorstack("No action: $action in $controller");
-      
+
       $instance->{$this->conf->defaultMethod}();
-    
+
     }
-  
+
   }
 
   function contextSwitch($context)
   {
     if($context == 'html'){
-      
+
       $this->register('context', 'html');
-    
+
     }
-  
+
   }
 
   function _request($var, $default = null)
@@ -290,7 +292,7 @@ class main
     if(null !== $default){
       return $default;
     }
-  
+
   }
 
   function setVars($parts)
@@ -299,115 +301,115 @@ class main
       if($k == 0 || ($k % 2) == 0){
         $this->_requestVars[$parts[$k]] = $parts[$k + 1];
       }
-    
+
     }
-  
+
   }
 
   private function hasController($parts, $module)
   {
     if($module == "default"){
-      
+
       $this->path = $this->conf->library . "/";
-    
+
     }else{
-      
+
       $this->path = $this->conf->library . "/modules/" . $module . "/";
-    
+
     }
-    
+
     if(! empty($parts[0]) && file_exists($this->path . $parts[0] . ".php")){
-      
+
       return true;
-    
+
     }
-    
+
     return false;
-  
+
   }
 
   private function hasAction($parts, $controller, $module)
   {
-    
+
     if(! empty($parts[0]) && method_exists($this->load($controller, null, $this->path, false), $parts[0])){
-      
+
       return true;
-    
+
     }
-    
+
     return false;
-  
+
   }
 
   function register($name, $value)
   {
-    
+
     $_SESSION[$name] = $value;
-  
+
   }
 
   protected function stack($msg)
   {
-    
+
     $_SESSION['stack'][] = $msg;
-  
+
   }
 
   protected function stackFlush()
   {
-    
+
     // unset($_SESSION['stack']);
-  
+
   }
 
   protected function errorStack($msg)
   {
-    
+
     $_SESSION['error'][] = $msg;
-    
+
     // $this->stackFlush();
-  
+
   }
 
   protected function errorStackFlush()
   {
-    
+
     unset($_SESSION['error']);
-  
+
   }
 
   function get($index)
   {
     if(array_key_exists($index, $_SESSION)){
-      
+
       return $_SESSION[$index];
-    
+
     }
-  
+
   }
 
   function autoload($class)
   {
-    
+
     $path = __dir__ . "/";
-    
+
     if(! strstr($class, '\\')){
-      
+
       if(file_exists($path . $class . ".php")){
-        
+
         include_once $path . $class . ".php";
-      
+
       }
       return;
     }
     // echo "loadin $path$class.php <br />";
     $parts = explode('\\', $class);
-    
+
     $i = 0;
     if($parts[0] == '\\'){
       $i = 1;
     }
-    
+
     $count = count($parts);
     for($i; $i < $count; $i++){
       if($parts[$i] == 'Codup'){
@@ -418,151 +420,149 @@ class main
         break;
       }
       $path = $path . $parts[$i] . "/";
-    
+
     }
-    
+
     if(file_exists($path)){
       include_once $path;
     }
-  
+
   }
 
   function load($class, $params = null, $path = null, $inst = true)
   {
-    
+
     $newpath = $path;
-    
-    if(null == $newpath){
-      
+
+    if(null === $newpath){
+
       $newpath = __DIR__ . "/";
-    
+
     }
-    
+
     $incpath = $newpath . $class . ".php";
-    
+
     $hash = hash('adler32', $incpath);
-    
+
     if($this->isLoaded($hash)){
-      
+
       return $this->get($hash);
-    
+
     }
-    
+
     $serialisable = array('config', 'debug', 'tools', 'view', 'bootstrap');
-    
+
     if(! file_exists($incpath)){
-      
+
       return false;
-    
+
     }
-    
+
     include_once ($incpath);
-    
-    $parameters = "";
-    
-    if(null !== $params && is_array($params)){
-      
-      $parameters = implode(",", $params);
-    
-    }else{
-      
-      $parameters = $params;
-    
-    }
-    
+
     if(false === $inst){
       return $class;
     }
-    
-    $instance = $class::getInstance($parameters);
-    
-    if(in_array($class, $serialisable)){
-      
-      $this->register($hash, $instance);
-    
+
+    $parameters = "";
+
+    if(null !== $params && is_array($params)){
+
+      $parameters = implode(",", $params);
+
+    }else{
+
+      $parameters = $params;
+
     }
-    
+
+    $instance = $class::getInstance($parameters);
+
+    if(in_array($class, $serialisable)){
+
+      $this->register($hash, $instance);
+
+    }
+
     return $instance;
-  
+
   }
 
   function isLoaded($hash)
   {
     if(isset($_SESSION[$hash])){
-      
+
       return true;
-    
+
     }
-    
+
     return false;
-  
+
   }
 
   function __set($var, $val)
   {
-    
+
     $this->vars[$var] = $val;
-  
+
   }
 
   function __get($var)
   {
-    $this->Stack($var);
-    
+
     switch($var){
-      
+
       case 'view':
-        
+
         return $this->load('view');
-        
+
         break;
-      
+
       case 'route':
-        
+
         return $this->get('route');
-        
+
         break;
-      
+
       case 'content':
-        
+
         return $this->viewPath;
-        
+
         break;
-      
+
       case 'db':
-        
+
         return $this->load('db');
-        
+
         break;
-      
+
       case 'conf':
-        
+
         return $this->load('config');
-        
+
         break;
       case 'bootstrap':
-        
+
         return $this->load('bootstrap');
-        
+
         break;
       case 'tools':
-        
+
         return $this->load('tools');
-        
+
         break;
       case 'debug':
-        
+
         return $this->load('debug');
-        
+
         break;
-    
+
     }
-    // if(in_array($var, $this->vars)){
-    return $this->vars[$var];
-    // }
-  
+    if(key_exists($var, $this->vars)){
+        return $this->vars[$var];
+    }
+
   }
 
 }
-
 ?>
