8242bc8e43efb7a6e6b113e0a74e4942440bd241
hussein guettaf <ghoucine@gmail.com>
2014-03-09 00:55:54 +0100

    Name based file log (create as many logs as needed on the fly)

--- library/modules/dev/index.php
+++ library/modules/dev/index.php
@@ -3,14 +3,23 @@
 class index extends Phiber\controller
 {
 
-  function main()
+  public function main()
   {
 
     $this->view->message = "This is dev module controller - welcome to Phiber";
 
+    //$log2 = $this->getLog('file',array('second','g:\\logfile2.txt'),'secondlog');
+    //$this->get('log')->debug('hello from index!:'.__line__);
+
+   // $log2->error('hello from index!:'.__line__);
+
+
+   // $this->get('log')->debug('second hello from index!:'.__line__);
+
+    //$this->get('secondlog')->debug('second hello from index!:'.__line__);
   }
 
-  function action()
+  public function action()
   {
 
     $this->view->origin = __class__;
