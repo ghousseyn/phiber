dc168c6371d33697581cb0adc939a2a13d66d9d9
housseyn guettaf <ghoucine@gmail.com>
2014-06-14 11:31:52 +0200

    oosql updated, distinct() defined;

--- library/error.php
+++ library/error.php
@@ -16,7 +16,8 @@ class error
     set_exception_handler('Phiber\error::exception_handler');
     register_shutdown_function('Phiber\error::fatal_error_handler');
 
-    ini_set('display_errors', false);
+    error_reporting(E_ALL);
+    ini_set('display_errors', true);
     if(! ini_get('log_errors') && \config::$PHIBER_LOG){
       ini_set('log_errors', true);
     }
