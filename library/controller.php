d319e5e239a0f567f34551aa617603c5fd838601
Housseyn Guettaf <ghussein@coda-dz.com>
2016-01-24 09:00:07 +0000

    pass view object down to controllers

--- library/controller.php
+++ library/controller.php
@@ -4,17 +4,24 @@ class controller
 {
 
     protected $phiber;
+    protected $view;
 
     public function __construct(phiber $phiber)
     {
         $this->phiber = $phiber;
-        $this->phiber->view;
+        $this->view = $this->phiber->view;
     }
 
     public function __call($name, $arguments)
     {
         if (method_exists($this->phiber, $name)) {
             return call_user_func_array([$this->phiber, $name], $arguments);
+        }
+    }
+    public function __get($name)
+    {
+        if (property_exists($this->phiber, $name)) {
+            return $this->phiber->{$name};
         }
     }
 }
